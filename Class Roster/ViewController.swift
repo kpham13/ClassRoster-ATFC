7fbaa6abf8c1f8e40b3e90ed0595c81b0188babf
Kevin Pham <kevin@MacBook-Air.local>
2014-09-04 11:38:00 -0700

    UISearchBar, updated AddViewController UI

--- Class Roster/ViewController.swift
+++ Class Roster/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate {
     
     var teachers = [Person]()
     var students = [Person]()
     var classRoster = [[Person]]()
+    var filteredRoster = [Person]()
+    var searchDataSource = [Person]()
+    
     var defaultProfileImage = UIImage(named: "default")
     
     @IBOutlet weak var tableView: UITableView!
@@ -55,6 +58,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             
         }
         
+        searchArray()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -69,29 +73,47 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
-        return self.classRoster.count
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            return 1
+        } else {
+            return self.classRoster.count
+        }
     }
     
     func tableView(tableView: UITableView!, titleForHeaderInSection section: Int) -> String! {
-        switch section {
-        case 0:
-            return "Teachers"
-        default:
-            return "Students"
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            return ""
+        } else {
+            switch section {
+            case 0:
+                return "Teachers"
+            default:
+                return "Students"
+            }
         }
         
     }
     
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return self.classRoster[section].count
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            return self.filteredRoster.count
+        } else {
+            return self.classRoster[section].count
+        }
+        
     }
     
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
         
-        var personForRow = self.classRoster[indexPath.section][indexPath.row]
-        cell.textLabel.text = personForRow.fullName()
+        var personForRow : Person
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            personForRow = self.filteredRoster[indexPath.row]
+        } else {
+            personForRow = self.classRoster[indexPath.section][indexPath.row]
+        }
         
+        cell.textLabel!.text = personForRow.fullName()
         // ** Adjust imageView frame and size OR should I pop a UIImageView controller inside prototype cell. **
         // cell.imageView.frame = CGRectMake(0, 0, 32, 32)
         // cell.imageView.bounds = CGRectMake(0, 0, 32, 32)
@@ -110,13 +132,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
         if segue.identifier == "showDetail" {
-            var detailViewController = segue.destinationViewController as DetailViewController
-            let section = tableView.indexPathForSelectedRow().section
-            let row = tableView.indexPathForSelectedRow().row
+            let detailViewController = segue.destinationViewController as DetailViewController
             
-            var personForRow = self.classRoster[section][row]
+            if self.searchDisplayController.active {
+                let indexPath = self.searchDisplayController!.searchResultsTableView.indexPathForSelectedRow()!
+                var personForRow = self.filteredRoster[indexPath.row]
+                detailViewController.selectedPerson = personForRow
+            } else {
+                let section = tableView.indexPathForSelectedRow().section
+                let row = tableView.indexPathForSelectedRow().row
+                var personForRow = self.classRoster[section][row]
+                detailViewController.selectedPerson = personForRow
+            }
             
-            detailViewController.selectedPerson = personForRow
         }
         
     }
@@ -160,4 +188,41 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
+    func filterContentForSearchText(searchText: String/*, scope: String = "All"*/) {
+        self.filteredRoster = self.searchDataSource.filter({ (person: Person) -> Bool in
+            // let categoryMatch = (scope = "All") //|| (candy.category == scope)
+            let stringMatch = person.fullName().rangeOfString(searchText)
+            return /*categoryMatch &&*/ (stringMatch != nil)
+        })
+        
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
+        // let scopes = self.searchDisplayController!.searchBar.scopeButtonTitles as [String]
+        // let selectedScope = scopes[self.searchDisplayController!.searchBar.selectedScopeButtonIndex] as String
+        self.filterContentForSearchText(searchString /*, scope: selectedScope*/)
+        return true
+    }
+    
+    /*
+    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchScope searchOption: Int) -> Bool {
+        // let scope = self.searchDisplayController!.searchBar.scopeButtonTitles as [String]
+        self.filterContentForSearchText(self.searchDisplayController!.searchBar.text /*, scope: scope[searchOption]*/)
+        return true
+    }
+    */
+    
+    func searchArray() {
+        for personIndex in 0...(teachers.count - 1) {
+            var newPerson = self.teachers[personIndex]
+            self.searchDataSource.append(newPerson)
+        }
+        
+        for personIndex in 0...(students.count - 1) {
+            var newPerson = self.students[personIndex]
+            self.searchDataSource.append(newPerson)
+        }
+        
+    }
+
 }
